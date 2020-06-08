dbb70a107e62a12703d71699f217c4a3babde6b5
hanlove2020 <66577327+hanlove2020@users.noreply.github.com>
2020-06-07 17:40:08 -0700

    script, styles, words added

---
+++ script.js
@@ -0,0 +1,114 @@
+let words = window.wordsData;
+let postings = window.blogData;
+// let kahoots = window.kahootData;
+console.log(words);
+
+//마지막 3개 blog summary 가져오기
+let target = document.querySelector('div.blog-sum');
+console.log(target);
+
+for (let i = postings.length - 1; i >= postings.length - 3; i--) {
+
+  console.log(postings[i]);
+  getBlogSum(postings[i]);
+}
+
+function getBlogSum(posting) {
+  console.log('getblogsum');
+  let temp = document.querySelector('#temp-post-sum');
+  let tempPop = document.querySelector('#temp-post-pop');
+  // console.log(temp);
+  let newBlogSum = document.importNode(temp.content, true);
+  let newBlogPop = document.importNode(tempPop.content, true);
+
+  newBlogSum.querySelector('.post-title-sum').textContent = posting['post-title'];
+  newBlogPop.querySelector('.post-title-pop').textContent = posting['post-title'];
+
+  if (posting['post-img'] === '') {
+    newBlogSum.querySelector('img.post-img-sum').src = 'images/hanlove_logo.jpg';
+    newBlogPop.querySelector('img.post-img-pop').src = 'images/hanlove_logo.jpg';
+  } else {
+    newBlogSum.querySelector('img.post-img-sum').src = 'images/' + posting['post-img'];
+    newBlogPop.querySelector('img.post-img-pop').src = 'images/' + posting['post-img'];
+
+  }
+
+  newBlogSum.querySelector('.post-desc-sum').textContent = posting['post-desc'];
+  newBlogPop.querySelector('.post-desc-pop').textContent = posting['post-desc'];
+  target.appendChild(newBlogSum);
+  target.appendChild(newBlogPop);
+}
+
+let postSumBtn = document.getElementsByClassName('post-sum-more');
+console.log(postSumBtn);
+
+for (let i = 0; i < postSumBtn.length; i++) {
+  postSumBtn[i].onclick = popPost;
+}
+
+function popPost() {
+  let postPop = this.parentNode.nextElementSibling;
+  console.log(postPop);
+  postPop.style.display = 'block';
+
+}
+
+
+
+
+
+//마지막 5개 단어 가져오기
+target = document.querySelector('div.word-inner-box');
+console.log(target);
+
+for (let i = words.length - 1; i >= words.length - 5; i--) {
+
+  console.log(i, '번째 단어', words[i]);
+  getWordsSum(words[i]);
+}
+
+function getWordsSum(word) {
+  console.log('getWordsum');
+  temp = document.querySelector('#temp-word-main');
+  console.log(temp);
+  let newWord = document.importNode(temp.content, true);
+
+  newWord.querySelector('.word-main').textContent = word['word'];
+  newWord.querySelector('.word-pop-word').textContent = word['word'];
+  newWord.querySelector('.word-pop-definition').textContent = '뜻 : ' + word['definition'];
+  if (word['example'] !== '') {
+    newWord.querySelector('.word-pop-example').textContent = '예문 : ' + word['example'];
+  }
+
+  target.appendChild(newWord);
+}
+
+let wordBtn = document.getElementsByClassName('word-main');
+console.log(wordBtn);
+
+for (let i = 0; i < wordBtn.length; i++) {
+  wordBtn[i].onclick = popWord;
+}
+
+function popWord() {
+  let wordPop = this.nextElementSibling;
+  console.log(wordPop);
+  wordPop.style.display = 'block';
+
+}
+
+//마지막 youtube가져오기 
+
+//모든 pop-up에 공통으로 사용될 close  (blog, word, youtube)
+let popClose = document.getElementsByClassName('pop-close');
+
+
+for (let i = 0; i < popClose.length; i++) {
+  popClose[i].onclick = closePop;
+}
+
+function closePop() {
+  let popWindow = this.parentNode;
+  popWindow.style.display = 'none';
+
+}
---
+++ wordsdata.js
@@ -0,0 +1,377 @@
+var wordsData = [{
+    'word': '남매',
+    'definition': '오빠와 누이를 아울러 이르는 말',
+    'example': '아빠와 고모는 남매다.'
+  },
+  {
+    'word': '보자기',
+    'definition': '물건을 싸서 들고 다닐 수 있도록 네모지게 만든 작은 천',
+    'example': '옛날에는 책가방 대신 보자기에 책들을 싸서 학교에 다녔다.'
+  },
+  {
+    'word': '차려입다',
+    'definition': '잘 갖추어 입다',
+    'example': '결혼식에 가려고 잘 차려입었다.'
+  },
+  {
+    'word': '어리둥절',
+    'definition': '무슨 일인지 잘 몰라서 얼떨떨한 모양',
+    'example': '왜 그런지 몰라서 어리둥절한 표정을 지었다.'
+  },
+  {
+    'word': '표지판',
+    'definition': '어떠한 사실을 알리기 위하여 일정한 표시를 해 놓은 판',
+    'example': '도로에는 길 이름이 적혀진 표지판이 있다.'
+  },
+  {
+    'word': '오두막집',
+    'definition': '사람이 겨우 들어가 살 정도로 작게 지은 집',
+    'example': '숲 속에 나뭇꾼이 살고 있는 오두막집이 있다.'
+  },
+  {
+    'word': '채',
+    'definition': '집을 세는 단위',
+    'example': '이 길 위에는 집이 모두 세 채 있다.'
+  },
+  {
+    'word': '다독이다',
+    'definition': '남의 약한 점을 따뜻이 어루만져 감싸고 달래다',
+    'example': ''
+  },
+  {
+    'word': '야릇하다',
+    'definition': '무엇이라고 표현할 수 없이 묘하고 이상하다',
+    'example': ''
+  },
+  {
+    'word': '질병',
+    'definition': '몸의 온갖 병',
+    'example': ''
+  },
+  {
+    'word': '부랴부랴',
+    'definition': '매우 급하게 서두르는 모양',
+    'example': ''
+  },
+  {
+    'word': '공손하다',
+    'definition': '말이나 행동이 겸손하고 예의 바르다',
+    'example': ''
+  },
+  {
+    'word': '고요하다',
+    'definition': '움직임이나 흔들림이 없이 잔잔하다',
+    'example': ''
+  },
+  {
+    'word': '귀하다',
+    'definition': '보물처럼 매우 소중하다',
+    'example': ''
+  },
+  {
+    'word': '동동',
+    'definition': '매우 안타깝거나 추워서 발을 가볍게 자꾸 구르는 모양',
+    'example': ''
+  },
+  {
+    'word': '휘둥그레지다',
+    'definition': '놀라거나 두려워서 눈이 크고 둥그렇게 되다',
+    'example': ''
+  },
+  {
+    'word': '빼꼼',
+    'definition': '작은 구멍이나 틈 사이로 아주 조금만 보이는 모양',
+    'example': ''
+  },
+  {
+    'word': '자명종',
+    'definition': '미리 정하여 놓은 시각이 되면 저절로 알리는 소리가 나도록 되어 있는 시계',
+    'example': '자명종 시계가 시끄럽게 울려서 잠에서 깨어났다.'
+  },
+  {
+    'word': '어엿하다',
+    'definition': '행동이 거리낌 없이 아주 당당하고 떳떳하다',
+    'example': '너도 이제 어엿한 중학생이 되었구나.'
+  },
+  {
+    'word': '학부모',
+    'definition': '학생의 아버지나 어머니라는 뜻으로, 학생의 보호자를 이르는 말',
+    'example': '엄마나 아빠 중에 한분은 학부모 회의에 오셔야 한다.'
+  },
+  {
+    'word': '한목소리',
+    'definition': '여럿이 함께 내는 하나의 목소리',
+    'example': '파티가 시작되자 모두들 한목소리로 함성을 질렀다.'
+  },
+  {
+    'word': '대견하다',
+    'definition': '흐뭇하고 자랑스럽다',
+    'example': ''
+  },
+  {
+    'word': '강조하다',
+    'definition': '어떤 부분을 특별히 강하게 주장하거나 두드러지게 하다',
+    'example': ''
+  },
+  {
+    'word': '신세',
+    'definition': '다른 사람에게 도움을 받거나 폐를 끼치는 일',
+    'example': ''
+  },
+  {
+    'word': '처제',
+    'definition': '아내의 여자 동생을 이르거나 부르는 말',
+    'example': ''
+  },
+  {
+    'word': '기어코',
+    'definition': '결국에 가서는',
+    'example': ''
+  },
+  {
+    'word': '이따가',
+    'definition': '조금 지난 뒤에',
+    'example': ''
+  },
+  {
+    'word': '형부',
+    'definition': '언니의 남편을 이르거나 부르는 말',
+    'example': ''
+  },
+  {
+    'word': '기세',
+    'definition': '기운차게 뻗치는 형세',
+    'example': ''
+  },
+  {
+    'word': '게걸음',
+    'definition': '게처럼 옆으로 걷는 걸음',
+    'example': ''
+  },
+  {
+    'word': '연하다',
+    'definition': '재질이 무르고 부드럽다',
+    'example': ''
+  },
+  {
+    'word': '골판',
+    'definition': '동물의 뼈와 같은 단단한 물질로 된 판',
+    'example': ''
+  },
+  {
+    'word': '자존심',
+    'definition': '남에게 굽히지 아니하고 자신의 품위를 스스로 지키는 마음',
+    'example': ''
+  },
+  {
+    'word': '육아낭',
+    'definition': '새끼를 넣어 기르는 주머니.  실고기나 해마류의 수컷 배에 있는 주머니',
+    'example': ''
+  },
+  {
+    'word': '요령',
+    'definition': '일을 하는 데 꼭 필요한 묘한 이치',
+    'example': ''
+  },
+  {
+    'word': '비밀',
+    'definition': '남에게 드러내거나 알리지 않기 위해 숨기는 일',
+    'example': ''
+  },
+  {
+    'word': '우리',
+    'definition': '짐승을 가두어 기르는 곳',
+    'example': ''
+  },
+  {
+    'word': '가로등',
+    'definition': '길에 달아 놓은 등',
+    'example': ''
+  },
+  {
+    'word': '꽁지',
+    'definition': '꼬리를 낮잡아 이르는 말',
+    'example': ''
+  },
+  {
+    'word': '서당',
+    'definition': '옛날 학교',
+    'example': ''
+  },
+  {
+    'word': '훈장',
+    'definition': '서당의 선생님',
+    'example': ''
+  },
+  {
+    'word': '단지',
+    'definition': '목이 짧고 가운데가 불룩한 작은 항아리',
+    'example': ''
+  },
+  {
+    'word': '죄인',
+    'definition': '죄를 지은 사람',
+    'example': ''
+  },
+  {
+    'word': '난처하다',
+    'definition': '이럴 수도 없고 저럴 수도 없어서 어렵다',
+    'example': ''
+  },
+  {
+    'word': '둥지',
+    'definition': '보금자리',
+    'example': ''
+  },
+  {
+    'word': '장례',
+    'definition': '죽은 사람을 땅에 묻어주는 일',
+    'example': ''
+  },
+  {
+    'word': '시끌벅적하다',
+    'definition': '많은 사람들이 어수선하게 움직이며 시끄럽다',
+    'example': ''
+  },
+  {
+    'word': '참가자',
+    'definition': '어떤 모임이나 단체 또는 일에 관련을 맺고 들어가는 사람',
+    'example': ''
+  },
+  {
+    'word': '비명',
+    'definition': '일이 매우 위급하거나 몹시 두려움을 느낄 때 지르는 소리',
+    'example': ''
+  },
+  {
+    'word': '부리',
+    'definition': '새의 앞쪽에 뾰족하게 튀어나온 부분',
+    'example': ''
+  },
+  {
+    'word': '촉수',
+    'definition': '몸의 구조가 간단한 동물의 몸 앞부분이나 입 주위에 튀어나와 있는 것 (예: 말미잘의 촉수)',
+    'example': ''
+  },
+  {
+    'word': '보호하다',
+    'definition': '위험하거나 어려운 일이 생기지 않도록 잘 보살펴 돌보다',
+    'example': ''
+  },
+  {
+    'word': '양분',
+    'definition': '영양이 되는 성분',
+    'example': ''
+  },
+  {
+    'word': '수상자',
+    'definition': '상을 받는 사람',
+    'example': ''
+  },
+  {
+    'word': '콧방귀를 뀌다',
+    'definition': '아니꼽거나 못마땅하여 남의 말을 들은 체 만 체 말대꾸를 하지 않다',
+    'example': ''
+  },
+  {
+    'word': '무기',
+    'definition': '전쟁할 때 쓰이는 물건',
+    'example': ''
+  },
+  {
+    'word': '망설이다',
+    'definition': '이리저리 생각만 하고 마음을 정하지 못하다',
+    'example': ''
+  },
+  {
+    'word': '흩어지다',
+    'definition': '한데 모였던 것이 따로따로 떨어지거나 여기저기로 퍼지다',
+    'example': '바람에 민들레 꽃씨가 흩어지고 있다.'
+  },
+  {
+    'word': '허둥지둥',
+    'definition': '정신을 차릴 수 없을 만큼 갈팡질팡하여 다급하게 서두르는 모양',
+    'example': '바쁘다고 그렇게 허둥지둥하면 실수하기 쉽다.'
+  },
+  {
+    'word': '불끈',
+    'definition': '물체 따위가 두드러지게 치밀거나 솟아오르거나 떠오르는 모양',
+    'example': ''
+  },
+  {
+    'word': '외양간',
+    'definition': '소를 키우는 곳',
+    'example': '소 잃고 외양간 고친다.'
+  },
+  {
+    'word': '계곡',
+    'definition': '물이 흐르는 골짜기',
+    'example': '비가 안와서 요세미티의 계곡에 물이 흐르지 않는다.'
+  },
+  {
+    'word': '기준',
+    'definition': '기본이 되는 표준',
+    'example': ''
+  },
+  {
+    'word': '목숨',
+    'definition': '사람이나 동물이 숨을 쉬며 살아 있는 힘.',
+    'example': '목숨이 아깝거든 내가 하는 말을 잘 들어라.'
+  },
+  {
+    'word': '고백하다',
+    'definition': '마음속에 생각하고 있는 것이나 감추어 둔 것을 사실대로 숨김없이 말하다.',
+    'example': ''
+  },
+  {
+    'word': '싫증',
+    'definition': '싫은 생각이나 느낌. 또는 그런 반응.',
+    'example': ''
+  },
+  {
+    'word': '감염',
+    'definition': '병원체인 미생물이 동물이나 식물의 몸 안에 들어가 증식하는 일.',
+    'example': ''
+  },
+  {
+    'word': '심각하다',
+    'definition': '상태나 정도가 매우 깊고 중대하다. 또는 절박함이 있다.',
+    'example': '용왕님의 병세가 심각하였다.'
+  },
+  {
+    'word': '중요하다',
+    'definition': '귀중하고 요긴하다.',
+    'example': ''
+  },
+  {
+    'word': '상상',
+    'definition': '실제로 경험하지 않은 현상이나 사물에 대하여 마음속으로 그려 봄',
+    'example': ''
+  },
+  {
+    'word': '노력하다',
+    'definition': '목적을 이루기 위하여 몸과 마음을 다하여 애를 쓰다.',
+    'example': '나는 시험을 잘 보기 위해서 열심히 노력했다.'
+  },
+  {
+    'word': '가볍다',
+    'definition': '무게가 일반적이거나 기준이 되는 대상의 것보다 적다',
+    'example': ''
+  },
+  {
+    'word': '역사',
+    'definition': '인류 사회의 변천과 흥망의 과정. 또는 그 기록',
+    'example': ''
+  },
+  {
+    'word': '대화',
+    'definition': '마주 대하여 이야기를 주고받음',
+    'example': ''
+  },
+  {
+    'word': '살랑거리다',
+    'definition': '조금 사늘한 바람이 가볍게 자꾸 불다',
+    'example': ''
+  }
+
+]
